11a9c3fec623667b0fd18745ba0f60251f57428b
Jason Duerstock <jason.duerstock@gallaudet.edu>
2008-09-25 18:51:26 -0400

    directory fully parsed except for extras

--- s002.asm
+++ s002.asm
@@ -25,59 +25,64 @@
 	DirEnt	$4000,2,1,429,$1000,21	; picture 4
 	DirEnt	$4000,2,1,473,$1000,21	; picture 5
 	DirEnt	$4000,2,1,517,$1000,21	; picture 6
-	.DBYT	$4021,112		; ???
-	.DBYT	$4a31,5			; unknown 1
-	.DBYT	$4a3c,$f000 | 32	; ???
-	.DBYT	$5001,56		; combat
-	.DBYT	$5072,42		; items
-	.DBYT	$4c01,17		; fountain
-	.DBYT	$4a7f,6			; ???
-	.DBYT	$4a8c,22		; ???
-	.DBYT	$5401,16		; vault
-	.DBYT	$5422,28		; goblin/troll
-	.DBYT	$545b,17		; acrinimiril's tomb
-	.DBYT	$4102,26		; ???
-	.DBYT	$4c24,28		; ???
-	.DBYT	$547e,28		; ozob
-	.DBYT	$54b7,39		; guild
-	.DBYT	$5506,32		; shop
-	.DBYT	$4c5d,20		; ???
-	.DBYT	$50c7,18		; inn
-	.DBYT	$5547,27		; oracle
-	.DBYT	$557e,24		; enchantress
-	.DBYT	$4137,32		; ???
-	.DBYT	$4c86,16		; ferryman
-	.DBYT	$4ca7,15		; horse
-	.DBYT	$4cc6,32		; smithy
-	.DBYT	$4d07,34		; brewery
-	.DBYT	$4d4c,16		; crypt
-	.DBYT	$4d6d,21		; dragon
-	.DBYT	$4d98,11		; ??? (death's door?)
-	.DBYT	$4178,37		; ???
-	.DBYT	$4daf,31		; machine room
-	.DBYT	$4dee,7			; elevator
-	.DBYT	$55af,31		; tavern
-	.DBYT	$50ec,176		; ???
-	.DBYT	$41c3,7			; ???
-	.DBYT	$4dfd,4			; unknown 2
-	.DBYT	$41d2,6			; ???
-	.DBYT	$4e06,$8000 | 8		; unknown 3
-	.DBYT	$55ee,$8000 | 7		; picture 7
-	.DBYT	$55fe,$8000 | 7		; picture 8
-	.DBYT	$524d,$0000 | 7		; dead song
-	.DBYT	$41df,0			; ???
-	.DBYT	$560e,13		; tavern monster song
-	.DBYT	$5629,6			; devourer song
-	.DBYT	$5636,4			; unknown song
-	.DBYT	$563f,23		; tavern talk
-	.DBYT	$41e0,18		; ???
-	.DBYT	$4205,79		; portal 2
-	.DBYT	$42a4,1			; ???
-	.DBYT	$42a7,1			; ???
-	.DBYT	$525c,$f000 | 32	; location names
-	.DBYT	$566e,16		; lost song
-	.DBYT	$568f,14		; black heart song
-	.DBYT	$4e18,5			; ???
-	.DBYT	$529f,5			; unknown 4
-	.DBYT	$56ac,5			; unknown 5
+	DirEnt	$4000,1,1,33,,112	; ???
+	DirEnt	$4000,2,1,561,,5	; unknown 1
+	DirEnt	$4000,2,1,572,$f000,32	; ???
+	DirEnt	$4000,3,1,1,,56		; combat
+	DirEnt	$4000,3,1,114,,42	; items
+	DirEnt	$4000,2,2,1,,17		; fountain
+	DirEnt	$4000,2,1,639,,6	; ???
+	DirEnt	$4000,2,1,652,,22	; ???
+	DirEnt	$4000,3,2,1,,16		; vault
+	DirEnt	$4000,3,2,34,,28	; goblin/troll
+	DirEnt	$4000,3,2,91,,17	; acrinimiril's tomb
+	DirEnt	$4000,1,1,258,,26	; ???
+	DirEnt	$4000,2,2,36,,28	; ???
+	DirEnt	$4000,3,2,126,,28	; ozob
+	DirEnt	$4000,3,2,183,,39	; guild
+	DirEnt	$4000,3,2,262,,32	; shop
+	DirEnt	$4000,2,2,93,,20	; ???
+	DirEnt	$4000,3,1,199,,18	; inn
+	DirEnt	$4000,3,2,327,,27	; oracle
+	DirEnt	$4000,3,2,382,,24	; enchantress
+	DirEnt	$4000,1,1,311,,32	; ???
+	DirEnt	$4000,2,2,134,,16	; ferryman
+	DirEnt	$4000,2,2,167,,15	; horse
+	DirEnt	$4000,2,2,198,,32	; smithy
+	DirEnt	$4000,2,2,263,,34	; brewery
+	DirEnt	$4000,2,2,332,,16	; crypt
+	DirEnt	$4000,2,2,365,,21	; dragon
+	DirEnt	$4000,2,2,408,,11	; ??? (death's door?)
+	DirEnt	$4000,1,1,376,,37	; ???
+	DirEnt	$4000,2,2,431,,31	; machine room
+	DirEnt	$4000,2,2,494,,7	; elevator
+	DirEnt	$4000,3,2,431,,31	; tavern
+	DirEnt	$4000,3,1,236,,176	; ???
+	DirEnt	$4000,1,1,451,,7	; ???
+	DirEnt	$4000,2,2,509,,4	; unknown 2
+	DirEnt	$4000,1,1,466,,6	; ???
+	DirEnt	$4000,2,2,518,$8000,8	; unknown 3
+	DirEnt	$4000,3,2,494,$8000,7	; unknown 3
+	DirEnt	$4000,3,2,510,$8000,7	; picture 7
+	DirEnt	$4000,3,1,589,,7	; picture 8
+	DirEnt	$4000,1,1,479,,0	; ???
+	DirEnt	$4000,3,2,526,,13	; tavern monster song
+	DirEnt	$4000,3,2,553,,6	; devourer song
+	DirEnt	$4000,3,2,566,,4	; unknown song
+	DirEnt	$4000,3,2,575,,23	; tavern talk
+	DirEnt	$4000,1,1,480,,18	; ???
+	DirEnt	$4000,1,1,517,,79	; portal 2
+	DirEnt	$4000,1,1,676,,1	; ???
+	DirEnt	$4000,1,1,679,,1	; ???
+	DirEnt	$4000,3,1,604,$f000,32	; ???
+	DirEnt	$4000,3,2,622,,16	; lost song
+	DirEnt	$4000,3,2,655,,14	; black heart song
+	DirEnt	$4000,2,2,536,,5	; ???
+	DirEnt	$4000,3,1,671,,5	; unknown 4
+	DirEnt	$4000,3,2,684,,5	; unknown 5
+;	.DBYT	$566e,16		; lost song
+;	.DBYT	$568f,14		; black heart song
+;	.DBYT	$4e18,5			; ???
+;	.DBYT	$529f,5			; unknown 4
+;	.DBYT	$56ac,5			; unknown 5
 	.RES	236,$00
